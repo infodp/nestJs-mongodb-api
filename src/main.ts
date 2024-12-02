fb4a3a22bb6de01223e8f8fbc54e58f948b1f5f9
infodp <diprimiogustavo@gmail.com>
2024-12-02 08:25:25 -0300

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
   //set del prefijo
-  app.setGlobalPrefix('api');
+  //app.setGlobalPrefix('api');
 
   //habilitamos validaciones
   app.useGlobalPipes(new ValidationPipe({
@@ -14,6 +14,16 @@ async function bootstrap() {
     whitelist: true,
     forbidNonWhitelisted: true
   }));
+
+  //swagger
+  const config = new DocumentBuilder()
+    .setTitle('Students API')
+    .setDescription('CRUD')
+    .setVersion('1.0')
+    .addTag('students')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   await app.listen(process.env.PORT ?? 3000);
 }
