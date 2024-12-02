dfe3f3f20f167e97956c51e9f6b1158522700e27
infodp <diprimiogustavo@gmail.com>
2024-12-02 16:16:54 -0300

    add swagger ui

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,16 @@ async function bootstrap() {
     whitelist: true,
     forbidNonWhitelisted: true
   }));
+
+  const config = new DocumentBuilder()
+                .setTitle('Students API')
+                .setDescription('Student CRUD')
+                .setVersion('1.0')
+                .addTag('students')
+                .build();
+  const documentFactory = ()=> SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
 
   await app.listen(process.env.PORT ?? 3000);
 }
