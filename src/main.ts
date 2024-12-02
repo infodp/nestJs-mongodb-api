3a4df6fbfe58800e7b547a77027ef4c0c3a426bb
infodp <diprimiogustavo@gmail.com>
2024-12-02 16:01:59 -0300

    del swagger

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,16 +13,6 @@ async function bootstrap() {
     whitelist: true,
     forbidNonWhitelisted: true
   }));
-
-  //swagger
-  const config = new DocumentBuilder()
-    .setTitle('Students API')
-    .setDescription('CRUD')
-    .setVersion('1.0')
-    .addTag('students')
-    .build();
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
 
   await app.listen(process.env.PORT ?? 3000);
 }
